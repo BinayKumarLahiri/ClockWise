f62aa69f553952845e8b79fb5c45ede795f30125
BinayKumarLahiri <binaykumarlahiri@gmail.com>
2024-04-21 00:40:19 +0530

    Created the basic UI and implemented the Clock featur

---
+++ index.js
@@ -0,0 +1,24 @@
+function clear() {
+    const btns = document.querySelectorAll("i");
+    btns.forEach(btn=>{
+        btn.style.backgroundColor = "var(--nav)";
+    })
+}
+document.getElementById("stop").addEventListener('click',()=>{
+    document.querySelector(".activebox").classList.remove("activebox");
+    document.querySelector(".stopBox").classList.add("activebox");
+    clear();
+    document.getElementById("stop").style.backgroundColor = "var(--background)";
+})
+document.getElementById("clock").addEventListener('click',()=>{
+    document.querySelector(".activebox").classList.remove("activebox");
+    document.querySelector(".clockBox").classList.add("activebox");
+    clear();
+    document.getElementById("clock").style.backgroundColor = "var(--background)";
+})
+document.getElementById("timer").addEventListener('click',()=>{
+    document.querySelector(".activebox").classList.remove("activebox");
+    document.querySelector(".timerBox").classList.add("activebox");
+    clear();
+    document.getElementById("timer").style.backgroundColor = "var(--background)";
+})
